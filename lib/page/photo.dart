1d3b06f88129633a7e72f340f2c002d4b315c07f
EqiBaihaqi <baihaqi08.aa@gmail.com>
2023-12-08 19:49:25 +0700

    update

--- lib/page/photo.dart
+++ lib/page/photo.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:proyek_ktp_3e/page/scan.dart';
