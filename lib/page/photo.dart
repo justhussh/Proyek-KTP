5a3e8fab22192a35a59295171bf1828e9e04d64a
EqiBaihaqi <baihaqi08.aa@gmail.com>
2023-12-19 13:47:53 +0700

    update

--- lib/page/photo.dart
+++ lib/page/photo.dart
@@ -47,87 +47,91 @@ class _PhotoState extends State<Photo> {
             ],
           ),
         ),
-        body: Column(children: [
-          Center(
-            child: Container(
-              margin: const EdgeInsets.only(top: 7),
-              constraints: const BoxConstraints(maxWidth: 300),
-              child: Text(
-                'Point the camera at the card you want to verify, make sure the image is clear',
-                style: TextStyle(
-                    fontSize: 15, color: Colors.black.withOpacity(0.6)),
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          _selectedImages != null
-              ? Center(
-                  child: Container(
-                      height: 500,
-                      width: 330,
-                      margin: const EdgeInsets.symmetric(
-                          horizontal: 15, vertical: 5),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(20),
-                        child: Image.file(
-                          _selectedImages!,
-                          fit: BoxFit.contain,
-                        ),
-                      )),
-                )
-              : Container(
-                  margin:
-                      const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-                  child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Image.asset('assets/noimage.jpg')),
+        body: SingleChildScrollView(
+          physics: const NeverScrollableScrollPhysics(),
+          child: Column(children: [
+            Center(
+              child: Container(
+                margin: const EdgeInsets.only(top: 7),
+                constraints: const BoxConstraints(maxWidth: 300),
+                child: Text(
+                  'Point the camera at the card you want to verify, make sure the image is clear',
+                  style: TextStyle(
+                      fontSize: 15, color: Colors.black.withOpacity(0.6)),
+                  textAlign: TextAlign.center,
                 ),
-          const SizedBox(
-            height: 30,
-          ),
-          GestureDetector(
-            onTap: () async {
-              final bb = await get(Uri.parse("http://192.168.69.11:5000"));
-              print(json.decode(bb.body));
-            },
-            child: Container(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 139, vertical: 20),
-              decoration: BoxDecoration(
-                  color: Colors.blue, borderRadius: BorderRadius.circular(10)),
-              child: const Text(
-                'Submit',
-                style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white),
               ),
             ),
-          ),
-          GestureDetector(
-            onTap: () {
-              _pickIMage(ImageSource.gallery);
-            },
-            child: Container(
-              margin: const EdgeInsets.only(top: 10),
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 113, vertical: 16),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(color: Colors.blue, width: 2)),
-              child: const Text(
-                'Upload Photo',
-                style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.blue),
+            const SizedBox(
+              height: 5,
+            ),
+            _selectedImages != null
+                ? Center(
+                    child: Container(
+                        height: 500,
+                        width: 330,
+                        margin: const EdgeInsets.symmetric(
+                            horizontal: 15, vertical: 5),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Image.file(
+                            _selectedImages!,
+                            fit: BoxFit.contain,
+                          ),
+                        )),
+                  )
+                : Container(
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 15, vertical: 15),
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Image.asset('assets/noimage.jpg')),
+                  ),
+            const SizedBox(
+              height: 30,
+            ),
+            GestureDetector(
+              onTap: () async {
+                final bb = await get(Uri.parse("http://192.168.69.11:5000"));
+                print(json.decode(bb.body));
+              },
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 139, vertical: 20),
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(10)),
+                child: const Text(
+                  'Submit',
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
               ),
             ),
-          ),
-        ]),
+            GestureDetector(
+              onTap: () {
+                _pickIMage(ImageSource.gallery);
+              },
+              child: Container(
+                margin: const EdgeInsets.only(top: 10),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 113, vertical: 16),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: Colors.blue, width: 2)),
+                child: const Text(
+                  'Upload Photo',
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue),
+                ),
+              ),
+            ),
+          ]),
+        ),
       ),
     );
   }
